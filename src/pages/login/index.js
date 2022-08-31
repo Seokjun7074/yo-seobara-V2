34f12c0e04aa36d9af3ea9a1b9ee7a3dd934a447
jinaSE0 <109025674+jinaSE0@users.noreply.github.com>
2022-08-31 19:15:04 +0900

    로그인 회원가입 페이지 post 요청까지 완료

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import "../login/index.css";
 import { useState, useRef } from "react";
+import { apis } from "../../api/loginAPI";
 
 const Login = (props) => {
   const [login, setLogin] = useState(true);
@@ -10,14 +11,43 @@ const Login = (props) => {
   const passwordConfirmInput = useRef();
   const nicknameInput = useRef();
 
-  const submitHandler =(event)=>{
-      event.prevenDefault();
+  const submitHandler =async(event)=>{
+      event.preventDefault();
 
-      // const enteredEmail = emailInputRef.current.value;
-      // const enteredPassword = passwordInputRef.current.value;
-      // const enteredPasswordConfirm = passwordConfirmInputRef.current.value;
-      // const enteredNickname = nicknameInputRef.current.value;
       
+      if (login){
+        const enteredEmail = emailInput.current.value;
+        const enteredPassword = passwordInput.current.value;
+
+        //로그인 post 요청
+        const userObj={
+          "username": enteredEmail,
+          "password": enteredPassword
+          }
+          const res =await apis.loginUser(userObj)
+          console.log(res)
+        
+      } else{
+        const enteredEmail = emailInput.current.value;
+        const enteredPassword = passwordInput.current.value;
+        const enteredPasswordConfirm = passwordConfirmInput.current.value;
+        const enteredNickname = nicknameInput.current.value;
+        
+        //회원가입 post 요청
+        const userObj={
+          "username": enteredEmail,
+          "password": enteredPassword,
+          "passwordCheck": enteredPasswordConfirm,
+          "nickname": enteredNickname,
+          "nicknameCheck": enteredNickname,
+          }
+          const res =await apis.registerUser(userObj)
+          console.log(res)
+          console.log(enteredEmail, enteredPassword, enteredPasswordConfirm, enteredNickname);
+      }
+
+        
+        
       
 
   };
@@ -31,10 +61,10 @@ const Login = (props) => {
       <div className="contentsWrap">
         <div className="loginWindow">
           {login? 
-            //<form onSubmit={submitHandler}>
+            <form onSubmit={submitHandler}>
               <div>
                 <div className="loginvalues">
-                <input type="text" placeholder="이메일" className="inlineToBlock" ref={emailInput}></input>
+                <input type="email" placeholder="이메일" className="inlineToBlock" ref={emailInput}></input>
                 <input type="text" placeholder="비밀번호" className="inlineToBlock" ref={passwordInput}></input>
                 <button className="inlineToBlock ordinaryLogin unactivatedLoginColor">로그인</button> 
                 </div>
@@ -42,22 +72,23 @@ const Login = (props) => {
               <div >
                 <p>
                   계정이 없으신가요?
-                  <button onClick={()=>{setLogin(false)}} class="noneunderline" className="haveAccount">가입하기</button>
+                  <button onClick={()=>{setLogin(false)}} className="haveAccount">가입하기</button>
                 </p>
               </div>
             </div>
-          // </form>
+          </form>
            : 
-          //<form onSubmit={submitHandler}>
+          <form onSubmit={submitHandler}>
             <div>
-              <input type="text" placeholder="이메일" className="inlineToBlock" ref={emailInput}></input>
+              <input type="email" placeholder="이메일" className="inlineToBlock" ref={emailInput}></input>
               <input type="text" placeholder="비밀번호" className="inlineToBlock" ref={passwordInput}></input>
               <input type="text" placeholder="비밀번호 확인" className="inlineToBlock" ref={passwordConfirmInput}></input>
               <input type="text" placeholder="닉네임" className="inlineToBlock" ref={nicknameInput}></input>
-              <button>닉네임 중복확인</button>
+              <button type="button">닉네임 중복확인</button>
+              <button className="inlineToBlock ordinaryLogin unactivatedLoginColor">회원가입</button>
               <button onClick={()=>{setLogin(true)}}>뒤로가기</button>
             </div>
-          //</form>
+          </form>
            }
         </div>
       </div>
