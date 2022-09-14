ffbce447b7e79e15e8cc3cd2f93be88b6a6c303d
이석준 <sukjune96@gmail.com>
2022-09-14 22:27:17 +0900

    fix: 무한스크롤 수정 & 그리드 배열 수정

--- src/pages/mainPhoto/index.js
+++ src/pages/mainPhoto/index.js
@@ -1,40 +1,14 @@
 import React from "react";
-import { StyledComponent } from "styled-components";
-import Card from "../../components/Card";
+import InfiniteScroll from "../../components/infinifeScrollTest";
+import Header from "../../components/global/header";
 
 const MainPhoto = () => {
   return (
     <div>
-      <Card />
+      <Header />
+      <InfiniteScroll />
     </div>
   );
 };
 
 export default MainPhoto;
-
-{
-  /* <div>사진만 나오는 메인페이지</div>
-             
-<div className="card-box">
-
-  <div className="card">
-   
-   <h1>사진보여주는공간(images)</h1>
-    <div className="contents">
-      <p>제목(title)</p>
-      <p>게시글내용(content)</p>
-      <p>장소(location)</p>
-        <div className="s-content">
-          <p>좋아요</p>
-          <p>조회수</p>
-          <p>작성된시간,수정된시간</p>
-        </div>
-
-
-    </div>
-
-
-</div>
-
-</div> */
-}
