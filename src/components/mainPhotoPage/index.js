bcedcb8656b4b8230d6bedebb8a5475dad32e72d
yc8569 <109015431+yc8569@users.noreply.github.com>
2022-09-14 23:02:39 +0900

    무한스크롤연습

--- src/components/mainPhotoPage/index.js
+++ src/components/mainPhotoPage/index.js
@@ -9,144 +9,224 @@ import { useInView } from "react-intersection-observer"
 import  Modal  from '../global/modal/index';
 import Detail from '../../pages/detail';
 import Slider from '../global/slider';
+import {getCookie} from '../../shared/Cookie';
+
+import styled from 'styled-components';
+
 
 const MainPhotoCard =()=> {
-
+  
 
 const navigate = useNavigate();
 
 const [datas, setDatas] = useState([]);
-const [page, setPage] = useState()
-  const [loading, setLoading] = useState(false)
-
-  const [ref, inView] = useInView()
-//   const [id, setId] = useState(1)
+const [page, setPage] = useState(0)
+const [loading, setLoading] = useState(false)
+const [ref, inView] = useInView(
+  {
+  threshold: 1, // ref부분이 다 보여야 작동
+  // triggerOnce: true, // 한번만 작동하는거 뺄지말지 고민중
+}
+);
  
   // 서버에서 아이템을 가지고 오는 함수
-  const getItems = useCallback(async () => {
-    setLoading(true)
-    await axios.get('http://13.209.88.83/api/posts')
-    .then((res) => {
-        setDatas(prevState => [...prevState, res.data])
-    })
-    .catch(err=> console.log(err))
-    setLoading(false)
-  }, [page])
+  // const getItems = useCallback(async () => {
+  //   setLoading(true)
+  //   await axios
+  //   // .get(`https://jsonplaceholder.typicode.com/photos/${page}`)
+  //   // .get(`http://13.209.88.83/api/posts?post=${page}`
+  //  .get('http://13.209.88.83/api/posts?page=2&size=4'
+  //   ,{
+  //     headers:{
+  //       Authorization : `Bearer ${getCookie("accessToken")}`
+     
+  
+  //     }
+  //   })
+  //   .then((res) => {
+  //     console.log(res.data.data.content)
+  //       // setDatas(prevState => [...prevState, res.data.data.content])
+  //       setDatas(res.data.data.content)
+  //   })
+
+  //   .catch(err=> console.log(err))
+  //   setLoading(false)
+  // }, [page])
 
 
-// useEffect(() => {
-//     axios.get('https://jsonplaceholder.typicode.com/photos')
-//       .then((res) => {
-//         setDatas(res.data);
-//       })
-//    .catch(err=> console.log(err))
 
-//   }, []);
+  const getItems = async () => {
+        console.log('서버통신 시작');
+        setLoading(true);
+        await axios
+          .get(`${process.env.REACT_APP_API_URL}/api/posts?page=${page}&size=4`, {
+            headers: {
+              Authorization: `Bearer ${getCookie('accessToken')}`,
+              
+            },
+          })
+          .then((res) => {
+            // console.log(“불러온 데이터“, res.data.data.last);
+            const dataList = res.data.data.content;
+            setDatas((prev) => [...prev, ...dataList]);
+          })
+          .catch((err) => console.log(err));
+        setLoading(false);
+      };
 
 
-  // `getItems` 가 바뀔 때 마다 함수 실행
+
+
+ // `page` 가 바뀔 때 마다 함수 실행
   useEffect(() => {
-    getItems()
-  }, [getItems])
+    getItems();
+  }, [page]);
 
 
   useEffect(() => {
-    
-    if (inView && !loading) {
-      setPage(prevState => prevState + 1)
-   
+    // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+    if (inView) {
+      setPage((prev) => prev + 1);
     }
-  }, [inView, loading])
+  }, [inView]);
+  // console.log(page);
+  // console.log(inView);
+
+
+
+  // useEffect(() => {
+    
+  //   if (inView && !loading) {
+  //     setPage(prevState => prevState + 1)
+   
+  //   }
+  // }, [inView, loading])
+
+
   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
 
 
 
-  
+  // console.log(datas.thumbnailUrl)
 
-  return (
-    <ImageList  sx={{ width: 1/1, height: 1/1 }}cols={3} rowMaxHeight={164} >
-      {datas.map((item, idx) => (
-        <React.Fragment key={idx}>     
-        
-            {datas.length -1 ==idx ? (
-              <div>
-                <ImageListItem key={item.url}>
-                <img onClick={()=>{
-                    navigate("/detail");
-                }}
-                        src={item.url}
-                    // src={`${item.url}?w=161&fit=crop&auto=format`}
-                    // srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
+
+return (
+  <ImageList  sx={{ width: 1/1, height: 1/1 }}cols={3} rowMaxHeight={164} >
+    {datas.map((item, idx) => (
+      <React.Fragment key={idx}>     
+      
+          {datas.length -1 ==idx ? (
+            <div>
+              <ImageListItem key={item.url}>
+              <img onClick={()=>{
+                  navigate("/detail");
+              }}
+                      src={item.thumbnailUrl}
+                  // src={`${item.url}?w=161&fit=crop&auto=format`}
+                  // srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                  // ref={ref}
+                  /> 
+                  <p>마지막</p>
+                  {datas.length === 0 ? null : <CheckBar ref={ref}></CheckBar>}
+                  <div>
+                    <ImageListItemBar
+                                      title={item.title}
+                                      subtitle={<span>{item.postId}: {item.author}</span>}
+                                      position="below"
+                                    />
+                    <Modal btn_title="상세페이지"  >
+                      <Detail id={item.id}/>
+                    </Modal>
+                  </div>
+                  </ImageListItem>
+                  
+                  </div>
+          ):(
+            <div className='Card'>
+            <ImageListItem key={item.url}>
+                <img 
+                       src={item.thumbnailUrl}
+                  //  src={`${item.url}?w=161&fit=crop&auto=format`}
+                  //   srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
                     alt={item.title}
                     loading="lazy"
-                    ref={ref}
-                    /> 
-                    <div>
-                      <ImageListItemBar
-                                        title={item.title}
-                                        subtitle={<span>by: {item.author}</span>}
-                                        position="below"
-                                      />
-                      <Modal btn_title="상세페이지"  >
+                  /> 
+                  {/* <Slider imageList={d_data}/> */}
+                  <div>
+                    <ImageListItemBar
+                                      title={item.title}
+                                      subtitle={<span>{item.postId}: {item.author}</span>}
+                                      position="below"
+                                    />
+                    <Modal btn_title="상세페이지" >
                         <Detail id={item.id}/>
-                      </Modal>
-                    </div>
-                    </ImageListItem>
-                    </div>
-            ):(
-              <div className='Card'>
-              <ImageListItem key={item.url}>
-                  <img 
-                         src={item.url}
-                    //  src={`${item.url}?w=161&fit=crop&auto=format`}
-                    //   srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
-                      alt={item.title}
-                      loading="lazy"
-                    /> 
-                    {/* <Slider imageList={d_data}/> */}
-                    <div>
-                      <ImageListItemBar
-                                        title={item.title}
-                                        subtitle={<span>by: {item.author}</span>}
-                                        position="below"
-                                      />
-                      <Modal btn_title="상세페이지" >
-                          <Detail id={item.id}/>
-                      </Modal>
-                    </div>
-                  <p>{item.id}</p>
-            </ImageListItem>
-            </div>
+                    </Modal> 
+                  </div>
+                <p>{item.id}</p>
                 
-            )}
-       
+          </ImageListItem>
+          
+          </div>
+              
+          )}
+     
+      </React.Fragment>
+    ))}
+    
+  </ImageList>
+);
 
-      
 
 
 
+  // return (
+  //   <ImageList  sx={{ width: 1/1, height: 1/1 }}cols={3} rowMaxHeight={164} >
+  //     {datas.map((item, idx) => (
+  //       <React.Fragment key={idx}>     
         
-        </React.Fragment>
-      ))}
-    </ImageList>
-  );
+  //       <div className='Card'>
+  //             <ImageListItem key={item.url}>
+  //                 <img 
+  //                        src={item.thumbnailUrl}
+  //                   //  src={`${item.url}?w=161&fit=crop&auto=format`}
+  //                   //   srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
+  //                     alt={item.title}
+  //                     loading="lazy"
+  //                   /> 
+  //                   {/* <Slider imageList={d_data}/> */}
+  //                   <div>
+  //                     <ImageListItemBar
+  //                                       title={item.title}
+  //                                       subtitle={<span>{item.postId}: {item.author}</span>}
+  //                                       position="below"
+  //                                     />
+  //                     <Modal btn_title="상세페이지" >
+  //                         <Detail id={item.id}/>
+  //                     </Modal>
+  //                   </div>
+  //                 <p>{item.id}</p>
+                  
+  //           </ImageListItem>
+            
+  //           </div>
+  //       </React.Fragment>
+  //     ))}
+  //     {datas.length === 0 ? null : <CheckBar ref={ref}></CheckBar>}
+  //   </ImageList>
+  // );
+
 }
 
 
 export default MainPhotoCard;
 
-// {
-//   “postId”: “게시물 번호”,
-//    "title": "게시글 제목",
-//    "content": "게시글 내용",
-//    "images": [이미지 파일]
-//    ”location”: { 
-//                        lat: 37.5614231, 
-//                        lng: 127.1911081 
-//                       }
-// },
-
-// variant="woven" cols={3} gap={3}
+const CheckBar = styled.div`
+  width: 100%;
+  height: 20px;
+  background-color: red;
+`;
 
 
 const itemData = [
@@ -218,3 +298,74 @@ const d_data = [
   'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
   'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
 ]
+
+
+
+
+ 
+// return (
+//   <ImageList  sx={{ width: 1/1, height: 1/1 }}cols={3} rowMaxHeight={164} >
+//     {datas.map((item, idx) => (
+//       <React.Fragment key={idx}>     
+      
+//           {datas.length -1 ==idx ? (
+//             <div>
+//               <ImageListItem key={item.url}>
+//               <img onClick={()=>{
+//                   navigate("/detail");
+//               }}
+//                       src={item.thumbnailUrl}
+//                   // src={`${item.url}?w=161&fit=crop&auto=format`}
+//                   // srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
+//                   alt={item.title}
+//                   loading="lazy"
+//                   // ref={ref}
+//                   /> 
+//                   {/* {datas.length === 0 ? null : <CheckBar ref={ref}></CheckBar>} */}
+//                   <div>
+//                     <ImageListItemBar
+//                                       title={item.title}
+//                                       subtitle={<span>{item.postId}: {item.author}</span>}
+//                                       position="below"
+//                                     />
+//                     <Modal btn_title="상세페이지"  >
+//                       <Detail id={item.id}/>
+//                     </Modal>
+//                   </div>
+//                   </ImageListItem>
+                  
+//                   </div>
+//           ):(
+//             <div className='Card'>
+//             <ImageListItem key={item.url}>
+//                 <img 
+//                        src={item.thumbnailUrl}
+//                   //  src={`${item.url}?w=161&fit=crop&auto=format`}
+//                   //   srcSet={`${item.url}?w=161&fit=crop&auto=format&dpr=2 2x`}
+//                     alt={item.title}
+//                     loading="lazy"
+//                   /> 
+//                   {/* <Slider imageList={d_data}/> */}
+//                   <div>
+//                     <ImageListItemBar
+//                                       title={item.title}
+//                                       subtitle={<span>{item.postId}: {item.author}</span>}
+//                                       position="below"
+//                                     />
+//                     <Modal btn_title="상세페이지" >
+//                         <Detail id={item.id}/>
+//                     </Modal>
+//                   </div>
+//                 <p>{item.id}</p>
+                
+//           </ImageListItem>
+          
+//           </div>
+              
+//           )}
+     
+//       </React.Fragment>
+//     ))}
+    
+//   </ImageList>
+// );
