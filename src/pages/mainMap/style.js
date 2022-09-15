98b6b67b80c3c3197af339d1fb9f8fc55487ce61
jinaSE0 <109025674+jinaSE0@users.noreply.github.com>
2022-09-15 20:57:35 +0900

    지도 스타일 충돌 수정

--- src/pages/mainMap/style.js
+++ src/pages/mainMap/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MainMapWrapper = styled.div`
   width: 100%;
-  height: 100%;
-  // display: flex;
-  // flex-direction: column;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
