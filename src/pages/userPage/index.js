83563f304b9a012b4bc16cefc50b667cdcc0d041
이석준 <sukjune96@gmail.com>
2022-09-22 15:32:20 +0900

    feat: userpage 데이터 받아오기 [#37]

--- src/pages/userPage/index.js
+++ src/pages/userPage/index.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { apis } from "../../api/postAPI";
+import Header from "../../components/global/header";
+import { UserPageWrapper } from "./style";
+
 const UserPage = () => {
-  return <div>유저페이지</div>;
+  // useParams로 이름 가져오기
+  const [data, setData] = useState();
+  const fetchData = async () => {
+    const res = await apis.getUserPost(0, "테스트");
+    console.log(res.data.data.content);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // console.log(data);
+  return (
+    <UserPageWrapper>
+      <Header></Header>
+      유저페이지
+    </UserPageWrapper>
+  );
 };
 
 export default UserPage;
